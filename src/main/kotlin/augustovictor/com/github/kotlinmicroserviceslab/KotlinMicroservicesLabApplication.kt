3b3b35019f3149d0cc2a0c6c02147759c7809b92
Victor Costa <victor.costa@creditas.com.br>
2018-09-20 13:02:08 -0300

    Initial structure

---
+++ src/main/kotlin/augustovictor/com/github/kotlinmicroserviceslab/KotlinMicroservicesLabApplication.kt
@@ -0,0 +1,11 @@
+package augustovictor.com.github.kotlinmicroserviceslab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMicroservicesLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinMicroservicesLabApplication>(*args)
+}
