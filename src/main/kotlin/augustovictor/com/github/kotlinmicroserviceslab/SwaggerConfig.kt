36c7b33bf9cbc789603a4d729e5ac27b17d251cc
Victor Costa <victor.costa@creditas.com.br>
2018-09-27 13:59:24 -0300

    [DOCS] Add swagger description to user attributes

--- src/main/kotlin/augustovictor/com/github/kotlinmicroserviceslab/SwaggerConfig.kt
+++ src/main/kotlin/augustovictor/com/github/kotlinmicroserviceslab/SwaggerConfig.kt
@@ -2,6 +2,8 @@ package augustovictor.com.github.kotlinmicroserviceslab
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -10,8 +12,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @EnableSwagger2
 class SwaggerConfig {
 
+    private final val DEFAULT_CONTACT = Contact("", "", "")
+    private final val DEFAULT_API_INFO = ApiInfo("My API!", "Customized Api description", "1.0", "http://terms-of-service-url.com", "Victor Augusto", "urn:tos", "http://license-url.com")
+
+    private val DEFAULT_PRODUCES_AND_CONSUMES = hashSetOf<String>("application/json", "application/xml")
+
     @Bean
     fun api(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
     }
 }
--- src/main/kotlin/augustovictor/com/github/kotlinmicroserviceslab/model/User.kt
+++ src/main/kotlin/augustovictor/com/github/kotlinmicroserviceslab/model/User.kt
@@ -1,20 +1,25 @@
 package augustovictor.com.github.kotlinmicroserviceslab.model
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
+@ApiModel(description = "This user is a customer from the system.")
 @Entity
 data class User(
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         val id: Int = 0,
 
+        @ApiModelProperty(notes = "Should have at least 2 characters")
         @NotNull
         @field:Size(min = 2)
         @Column(name = "first_name")
         val firstName: String = "",
 
+        @ApiModelProperty(notes = "Should have at least 2 characters")
         @NotNull
         @field:Size(min = 2, max = 10)
         @Column(name = "last_name")
